c51b8283fe47be4b46f9538f36202b870365841b
Swyter <swyterzone@gmail.com>
2024-04-10 01:25:40 +0200

    Firmware: Start untangling the RP2040 CMake build scripts on Windows, everything seems pretty cursed, like having to launch VS Code from a specific shortcut to configure the environment variables. Start messing around with I2C for the Type-C chip.

--- firmware/blink/blink.c
+++ firmware/blink/blink.c
@@ -9,6 +9,18 @@
 #include "hardware/gpio.h"
 #include "hardware/adc.h"
 
+#include "pico/binary_info.h"
+#include "hardware/i2c.h"
+
+
+// I2C reserves some addresses for special purposes. We exclude these from the scan.
+// These are any addresses of the form 000 0xxx or 111 1xxx
+
+bool reserved_addr(uint8_t addr) {
+    return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
+}
+ 
+
 int main() {
     stdio_init_all();
     stdio_usb_init();
@@ -30,6 +42,42 @@ int main() {
     adc_gpio_init(26);
     // Select ADC input 0 (GPIO26)
     adc_select_input(0);
+
+
+    // This example will use I2C0 on the default SDA and SCL pins (GP4, GP5 on a Pico)
+    i2c_init(i2c_default, 100 * 1000);
+    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+    // Make the I2C pins available to picotool
+    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
+ 
+    printf("\nI2C Bus Scan\n");
+    printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
+ 
+    for (int addr = 0; addr < (1 << 7); ++addr) {
+        if (addr % 16 == 0) {
+            printf("%02x ", addr);
+        }
+ 
+        // Perform a 1-byte dummy read from the probe address. If a slave
+        // acknowledges this address, the function returns the number of bytes
+        // transferred. If the address byte is ignored, the function returns
+        // -1.
+ 
+        // Skip over any reserved addresses.
+        int ret;
+        uint8_t rxdata;
+        if (reserved_addr(addr))
+            ret = PICO_ERROR_GENERIC;
+        else
+            ret = i2c_read_blocking(i2c_default, addr, &rxdata, 1, false);
+ 
+        printf(ret < 0 ? "." : "@");
+        printf(addr % 16 == 15 ? "\n" : "  ");
+    }
+    printf("Done.\n");
 
     while (1) {
         // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
