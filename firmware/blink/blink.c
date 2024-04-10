a6ee74002a7fcaa776484f0a0e7a912198698fae
Swyter <swyterzone@gmail.com>
2024-04-10 02:51:58 +0200

    Firmware: Add a "program" launch property, so that the VS Code CMake extension detects it's part of the project and links it to the default bug icon debug button. Disable the pull-ups, there are already external 4.7K pull-up resistors, in theory. I still don't know why it gets stuck on `i2c_read_blocking()`, yet. Hopefully it's fixable in software without bodges.

--- firmware/blink/blink.c
+++ firmware/blink/blink.c
@@ -45,18 +45,18 @@ int main() {
 
 
     // This example will use I2C0 on the default SDA and SCL pins (GP4, GP5 on a Pico)
-    i2c_init(i2c_default, 100 * 1000);
+    i2c_init(i2c_default, 100);
     gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
     gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
-    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+    //gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+    //gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
     // Make the I2C pins available to picotool
     bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
  
     printf("\nI2C Bus Scan\n");
     printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
  
-   /* for (int addr = 0; addr < (1 << 7); ++addr) {
+    for (int addr = 0; addr < (1 << 7); ++addr) {
         if (addr % 16 == 0) {
             printf("%02x ", addr);
         }
@@ -78,7 +78,7 @@ int main() {
         printf(addr % 16 == 15 ? "\n" : "  ");
     }
     printf("Done.\n");
-    */
+
 
     while (1) {
         // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
